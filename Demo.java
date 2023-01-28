aa64755c0c32c95fad414200e0ba138eb5b8a7d0
altertayo <tolofi152@gmail.com>
2023-01-28 01:43:19 -0500

    okay now i'm done

--- Demo.java
+++ Demo.java
@@ -3,21 +3,32 @@ import java.util.Scanner;
 public class Demo {
 
     public static void main(String[] args) {
-        
-        System.out.println("\n\nWelcome to the Music Player!\n\nWhich song would you like to listen to?\n\n(pick 1 or 2)\n1. Life Lesson - dodie\n2. Chinese New Year - SALES\n");
 
-        Scanner kb  = new Scanner(System.in);
-        //kb.nextLine();
+        System.out.println(
+                "\n\nWelcome to the Music Player!\n\nWhich song would you like to listen to?\n\n(pick 1 or 2)\n1. Life Lesson - dodie\n2. Chinese New Year - SALES\n");
+
+        Scanner kb = new Scanner(System.in);
+        // kb.nextLine();
         String input = kb.nextLine();
         Song s;
 
-        if(input.equals("1"))
-        {
+        if (input.equals("1")) {
             s = new Song1();
-        }
-        else
-        {
+        } else {
             s = new Song2();
+        }
+
+        while (!(input.equals("1")) && !(input.equals("2"))) {
+            System.out.println(
+                    "\nTry again \n\nWhich song would you like to listen to?\n\n(pick 1 or 2)\n1. Life Lesson - dodie\n2. Chinese New Year - SALES\n");
+
+            input = kb.nextLine();
+
+            if (input.equals("1")) {
+                s = new Song1();
+            } else {
+                s = new Song2();
+            }
         }
 
         System.out.print("\n\n\nNOW PLAYING : ");
@@ -33,7 +44,7 @@ public class Demo {
 
         s.printLyrics();
 
-        System.out.println("\n");
+        System.out.println();
 
         kb.close();
     }
